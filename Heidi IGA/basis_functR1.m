8fefb94ab75cecb8cd330cfc5402bfd7f3126d8c
heidiburger <36988261+heidiburger@users.noreply.github.com>
2018-03-15 14:24:31 +0200

    Add files via upload

--- Heidi IGA/basis_functR1.m
+++ Heidi IGA/basis_functR1.m
@@ -1,24 +1,25 @@
-function x_vec = basis_functR1(p,Xi,n,xi_vec,const_w,B)
+function x_vec = basis_functR1(p,Xi,n,xi_vec,w,B)
 %Where p is the order of the basis function
 %      Xi is the Knot vector
 %      n is the number of basis functions
-%      xi is the point at which you want to evaluate
-%      const_w is the NURBS constants 
+%      xi_vec is the point or vector at which you want to evaluate
+%      w is the NURBS constants 
+%      B is the control point polygon
 
 ppoints=length(xi_vec);
 
     x_vec=zeros(1,ppoints);
-    for jj = 1:ppoints          %Full points to plot NURBS loop
+    for ii_points = 1:ppoints          %Full points to plot NURBS loop
         c=0;
         W=0;
         
-        for j = 1:n         %Loop through basis functions
-            W = W + ( basis_funct(p,Xi,j,xi_vec(jj))*const_w(j) );
+        for ii = 1:n         %Loop through basis functions
+            W = W + ( basis_funct(p,Xi,ii,xi_vec(ii_points))*w(ii) );
         end
 
-        for i = 1:n         %Loop through basis functions
-            c = c + basis_funct(p,Xi,i,xi_vec(jj))*const_w(i)*B(i);
+        for ii = 1:n         %Loop through basis functions
+            c = c + basis_funct(p,Xi,ii,xi_vec(ii_points))*w(ii)*B(ii);
         end
-        x_vec(jj) = c/W;    %NURBS vector
+        x_vec(ii_points) = c/W;    %NURBS vector
     end
    
